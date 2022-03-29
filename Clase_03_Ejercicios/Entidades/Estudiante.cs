5693bff7c4a33abe82f4e513c3d5c115faf8335b
Mica Vazzana <micaelavazzana@hotmail.com>
2022-03-28 22:18:54 -0300

    subo ejercicios clase 3

---
+++ Clase_03_Ejercicios/Entidades/Estudiante.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Estudiante
+    {
+        private string apellido;
+        private string legajo;
+        private string nombre;
+        private int notaPrimerParcial;
+        private int notaSegundoParcial;
+        private static Random random;
+
+        static Estudiante()
+        {
+            Estudiante.random = new Random();
+        }
+
+        public Estudiante(string apellido, string legajo, string nombre)
+        {
+            this.apellido = apellido;
+            this.legajo = legajo;
+            this.nombre = nombre;
+        }
+
+
+    }
+}
