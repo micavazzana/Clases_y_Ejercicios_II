2e5490337ed284a0edf5832a49f0e2043788119d
Mica Vazzana <micaelavazzana@hotmail.com>
2022-04-01 14:48:48 -0300

    update de ejercicios clase 4

---
+++ Clase_04_Ejercicios/Ej_Estanteria/Estante.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_Estanteria
+{
+    public class Estante
+    {
+        private Producto[] productos;
+        private int ubicacionEstante;
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Ubicacion del estante: {e.ubicacionEstante}");
+
+            for (int i = 0; i < e.productos.Length; i++)
+            {
+                sb.AppendLine(Producto.MostrarProducto(e.productos[i]));
+            }
+            return sb.ToString();
+        }
+        public static bool operator ==(Estante e, Producto p)
+        {
+            for (int i = 0; i < e.productos.Length; i++)
+            {
+                if (p == e.productos[i])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+        public static bool operator +(Estante e, Producto p)
+        {
+            if (e != p)//si el producto no esta en el estante
+            {
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if(e.productos[i] is null)//si hay lugar
+                    {
+                        e.productos[i] = p;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public static Estante operator -(Estante e, Producto p)
+        {
+  
+            for (int i = 0; i < e.productos.Length; i++)
+            {
+                if(e == p)//si el producto existe
+                {
+                    e.productos[i] = null;
+                }
+            }
+            return e;
+        }
+    }
+}
---
+++ Clase_04_Ejercicios/Ej_Estanteria/Producto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Ej_Estanteria
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+        public Producto(string marca, string codigoDeBarra, float precio)
+        {
+            this.codigoDeBarra = codigoDeBarra;
+            this.marca = marca;
+            this.precio = precio;
+        }
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+        public static string MostrarProducto(Producto p)
+        {
+            return $"Marca: {p.GetMarca()} Codigo de barras: {p.codigoDeBarra} Precio: {p.GetPrecio()}";
+        }
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            if(!(p1 is null || p2 is null))
+                return (p1.GetMarca() == p2.GetMarca() && p1.codigoDeBarra == p2.codigoDeBarra);
+            return false;
+        }
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+        public static bool operator ==(Producto p, string marca)
+        {
+            return p.GetMarca() == marca;
+        }
+        public static bool operator !=(Producto p, string marca)
+        {
+            return !(p == marca);
+        }
+    }
+}
