90b863d843f072ef45f526a03737a914c640f202
Mica Vazzana <micaelavazzana@hotmail.com>
2022-06-14 16:25:21 -0300

    modelo parcial en proceso

---
+++ Modelos de parcial 2/Segundo.Parcial.Bomberos/Entidades/Bombero.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Xml.Serialization;
+
+namespace Entidades
+{
+    public delegate void FinDeSalida(int bomberoIndex);
+    public class Bombero : IArchivo<string>, IArchivo<Bombero>
+    {
+        private string nombre;
+        private List<Salida> salidas;
+        public event FinDeSalida MarcarFin;
+        private static string rutaBase;
+        private static Random random;
+
+        static Bombero()
+        {
+            Bombero.rutaBase = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            random = new Random();
+        }
+        public Bombero()
+        {
+            this.salidas = new List<Salida>();
+        }
+        public Bombero(string nombre) : this()
+        {
+            this.nombre = nombre;            
+        }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public List<Salida> Salidas { get => salidas; set => salidas = value; }
+        public void AtenderSalida(int bomberoIndex)
+        {
+            Salida nuevaSalida = new Salida();
+            this.salidas.Add(nuevaSalida);
+            Thread.Sleep(random.Next(2000,4000));
+            nuevaSalida.FinalizarSalida();
+            //Registrará la salida (horario de inicio, horario de fin y duración total)
+            //en la tabla log de la base de datos.
+            MarcarFin(bomberoIndex);
+        }
+
+        //Implementacion implicita. Bombero guarda en xml
+        public void Guardar(Bombero info)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter($"{rutaBase}\\Bombero.xml"))
+                {
+                    XmlSerializer xml = new XmlSerializer(typeof(Bombero));
+                    xml.Serialize(sw, info);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Error al serializar bombero",ex);
+            } 
+        }
+        public Bombero Leer()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader($"{rutaBase}\\Bombero.xml"))
+                {
+                    XmlSerializer xml = new XmlSerializer(typeof(Bombero));
+                    Bombero bombero = xml.Deserialize(sr) as Bombero;
+                    return bombero;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al serializar bombero", ex);
+            }
+        }
+
+        //Implementacion explicita
+        void IArchivo<string>.Guardar(string info)
+        {
+            throw new NotImplementedException();
+        }
+        string IArchivo<string>.Leer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Modelos de parcial 2/Segundo.Parcial.Bomberos/Entidades/Salida.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Salida
+    {
+        private DateTime fechaFin;
+        private DateTime fechaInicio;
+
+        public Salida()
+        {
+            this.fechaInicio = DateTime.Now;
+        }
+
+        public DateTime FechaFin { get => fechaFin; set => fechaFin = value; }
+        public DateTime FechaInicio { get => fechaInicio; set => fechaInicio = value; }
+        public double TiempoTotal
+        { 
+            get; 
+        }
+
+        public void FinalizarSalida()
+        {
+            this.fechaFin = DateTime.Now;
+        }
+    }
+    public static class DateTimeExtendido
+    { 
+        static void DiferenciaFechas(this DateTime fecha)
+        {
+
+        }
+    }
+}
